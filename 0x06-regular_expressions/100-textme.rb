3ca376b92df3e803e15f58ae70cb534abe1fb7c5
OttoNyongole <nyongole.otto@gmail.com>
2022-10-21 21:27:26 +0300

    textMe script

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,12 @@
 #!/usr/bin/env ruby
-Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
-List = [Format[0].compact, Format[1].compact, Format[2].compact]
-puts List.join(',')
+# The script should outpu: SENDER, RECEIVER, FLAGS
+# sender phone number or name (including country code if any)
+# The receiver phone number or name
+# Flags that were used
+
+$stdout.sync = true
+print ARGV[0].scan(/from:(\+?\d*)/).join
+print ","
+print ARGV[0].scan(/to:(\+?\d*)/).join
+print ","
+puts ARGV[0].scan(/flags:([:\-0-9]*)/).join
