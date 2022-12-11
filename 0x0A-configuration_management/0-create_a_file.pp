db1d4bab1257c82b81933ca5f1f13f637c8ecf7b
OttoNyongole <nyongole.otto@gmail.com>
2022-12-11 14:40:59 +0300

    Puppet style correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# Using Puooet, to create a file in /temp
-
-file{'school':
-ensure	==> present,
-path	==> '/temp/school',
-mode	==> '0744',
-owner	==> 'www-data',
-group	==> 'www-data',
-content	==> 'I love Puppet',
+# Using Puppet, create a file in /tmp
+file { 'school':
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
