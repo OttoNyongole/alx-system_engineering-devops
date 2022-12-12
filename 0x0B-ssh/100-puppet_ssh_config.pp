f020225f6f460bd57ad93973dac5f23abcbdb4a9
OttoNyongole <nyongole.otto@gmail.com>
2022-12-12 20:31:09 +0300

    edited file 100-puppet_ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,14 @@
-# Create the ~/.ssh/config with puppet
-file {'~/.ssh/config':
-  ensure  => present,
-  replace => 'yes',
-  path    => '~/.ssh/config',
-  content => 'Host *
-     HostName 100.25.194.59
-     User ubuntu
-     IdentityFile ~/.ssh/school',
-  mode    => '7000',
+# Client configuration file (w/Puppet)
+
+file_line { 'Turn off passwd auth':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+    match  => 'PasswordAuthentication yes'
+}
+
+file_line { 'Declare identity file':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/school'
 }
